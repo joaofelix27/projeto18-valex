5ee15bf255ccfc803c41cf2e8cc6caf4b7a08a0e
joaofelix27 <joaofelix27@gmail.com>
2022-09-05 18:00:56 -0300

    Final version without deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,9 +7,7 @@ const { Pool } = pg;
 
 const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-      rejectUnauthorized: false
-  }
+
 }
 const connection = new Pool(databaseConfig);
 
